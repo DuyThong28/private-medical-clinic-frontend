94b108f8089618586bce512de8971e7b1babe267
DuyThong28 <duythong28112004@gmail.com>
2024-05-16 12:13:38 +0700

    commit

--- src/services/drugReport.js
+++ src/services/drugReport.js
@@ -1,20 +1,23 @@
 export async function fetchAllDrugReportData() {
-    const response = await fetch("http://localhost:8080/api/v1/drugusagereports", {
+  const response = await fetch(
+    "http://localhost:8080/api/v1/drugusagereports",
+    {
       credentials: "include",
       headers: {
         "Content-Type": "application/json",
         authorization: "Bearer",
       },
-    });
-  
-    if (!response.ok) {
-      const error = new Error("An error occurred while fetching all the drugs");
-      error.code = response.status;
-      error.info = await response.json();
-      throw error;
     }
-  
-    const resData = await response.json();
-    const drugs = resData.drugs;
-    return drugs;
+  );
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching all the drugs");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
   }
+
+  const resData = await response.json();
+  const drugs = resData.data;
+  return drugs;
+}
